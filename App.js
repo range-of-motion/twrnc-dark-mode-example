b71a8081b649e7d170eaeb4ebd632b2ee654efbe
Daniël van Drunen <daniel@pixely.me>
2021-12-08 23:22:22 +0100

    Attempt to use twrnc's dark prefix

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import Settings from './Settings';
+import tw, { useDeviceContext } from 'twrnc';
 
 export default () => {
+  useDeviceContext(tw);
+
   return (
     <SafeAreaView>
       <Settings />
--- Settings.js
+++ Settings.js
@@ -4,12 +4,12 @@ import tw from 'twrnc';
 
 export default () => {
   const switchTheme = (theme) => {
-    alert(theme);
+    tw.setColorScheme(theme);
   };
 
   return (
-    <View style={tw`m-5 p-5 bg-red-100 rounded-lg`}>
-      <Text>Select a theme</Text>
+    <View style={tw`m-5 p-5 bg-gray-100 dark:bg-gray-900 rounded-lg`}>
+      <Text style={tw`text-black dark:text-white`}>Select a theme</Text>
       <View style={tw`flex-row mt-2 -mx-1`}>
         <TouchableOpacity style={tw`flex-1 mx-1 py-2 px-3 bg-white rounded-lg`} onPress={() => switchTheme('light')}>
           <Text>Light</Text>
